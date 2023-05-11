a4819a54c6dd41859bd521db941b28e546f21bc6
Dev2-dipanetech <dev2@dipanaetech.com>
2023-05-11 11:23:33 +0530

    some changes in gym member scripts

--- gms/gym_management_system/doctype/gym_member/gym_member.py
+++ gms/gym_management_system/doctype/gym_member/gym_member.py
@@ -3,14 +3,10 @@
 
 import frappe
 from frappe.website.website_generator import WebsiteGenerator
-from frappe.model.document import Document
-
-
-class GymMember(Document):
-    def before_insert(self):
+    
+class GymMember(WebsiteGenerator):
+    def before_save(self):
         if self.middle_name:
             self.full_name = self.first_name + ' ' + self.middle_name + ' ' + self.last_name
         else:
             self.full_name = self.first_name + ' ' + self.last_name
-class GymMember(WebsiteGenerator):
-    pass
